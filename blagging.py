8918dca6f1ec357a92531c380bf3af94ff6bedd5
Jeffrey Gomberg <jeffgomberg@yahoo.com>
2017-01-10 18:48:42 -0600

    started creating blagging balanced ensemble bagging classifier framework for extreme imbalanced classes

---
+++ blagging.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 10 07:44:05 2017
+
+@author: jgomberg
+"""
+
+import numpy as np
+import sklearn
+from sklearn.ensemble import BaggingClassifier
+from sklearn.utils.validation import has_fit_parameter
+from sklearn.utils.fixes import bincount
+from sklearn.utils import indices_to_mask
+
+__all__ = ["BlaggingClassifier"]
+
+MAX_INT = np.iinfo(np.int32).max
+
+def _generate_bagging_indices(random_state, bootstrap_features,
+                              bootstrap_samples, n_features, n_samples,
+                              max_features, max_samples):
+    """Randomly draw feature and sample indices."""
+    # Get valid random state
+    random_state = check_random_state(random_state)
+
+    # Draw indices
+    feature_indices = _generate_indices(random_state, bootstrap_features,
+                                        n_features, max_features)
+    sample_indices = _generate_indices(random_state, bootstrap_samples,
+                                       n_samples, max_samples)
+
+    return feature_indices, sample_indices
+
+
+def _parallel_build_estimators(n_estimators, ensemble, X, y, sample_weight,
+                       seeds, total_n_estimators, verbose):
+    """Private function used to build a batch of estimators within a job."""
+    # Retrieve settings
+    print("RIGHT CALL!!!")
+    n_samples, n_features = X.shape
+    max_features = ensemble._max_features
+    max_samples = ensemble._max_samples
+    bootstrap = ensemble.bootstrap
+    bootstrap_features = ensemble.bootstrap_features
+    support_sample_weight = has_fit_parameter(ensemble.base_estimator_,
+                                              "sample_weight")
+    if not support_sample_weight and sample_weight is not None:
+        raise ValueError("The base estimator doesn't support sample weight")
+
+    # Build estimators
+    estimators = []
+    estimators_features = []
+
+    for i in range(n_estimators):
+        if verbose > 1:
+            print("Building estimator %d of %d for this parallel run (total %d)..." %
+                  (i + 1, n_estimators, total_n_estimators))
+
+        random_state = np.random.RandomState(seeds[i])
+        estimator = ensemble._make_estimator(append=False,
+                                             random_state=random_state)
+
+        # Draw random feature, sample indices
+        features, indices = _generate_bagging_indices(random_state,
+                                                      bootstrap_features,
+                                                      bootstrap, n_features,
+                                                      n_samples, max_features,
+                                                      max_samples)
+
+        # Draw samples, using sample weights, and then fit
+        if support_sample_weight:
+            if sample_weight is None:
+                curr_sample_weight = np.ones((n_samples,))
+            else:
+                curr_sample_weight = sample_weight.copy()
+
+            if bootstrap:
+                sample_counts = bincount(indices, minlength=n_samples)
+                curr_sample_weight *= sample_counts
+            else:
+                not_indices_mask = ~indices_to_mask(indices, n_samples)
+                curr_sample_weight[not_indices_mask] = 0
+
+            estimator.fit(X[:, features], y, sample_weight=curr_sample_weight)
+
+        # Draw samples, using a mask, and then fit
+        else:
+            estimator.fit((X[indices])[:, features], y[indices])
+
+        estimators.append(estimator)
+        estimators_features.append(features)
+
+    return estimators, estimators_features
+
+class BlaggingClassifier(BaggingClassifier):
+
+    def __init__(self,
+                 base_estimator=None,
+                 n_estimators=10,
+                 max_samples=1.0,
+                 max_features=1.0,
+                 bootstrap=True,
+                 bootstrap_features=False,
+                 oob_score=False,
+                 warm_start=False,
+                 n_jobs=1,
+                 random_state=None,
+                 verbose=0):
+
+        # WARNING - this is dangerous - need to set this back if going to use any real Bagging ensembles through
+        #sklearn
+        self.orig_parallel_build_estimators = sklearn.ensemble.bagging._parallel_build_estimators
+        self.orig_generate_bagging_indices = sklearn.ensemble.bagging._generate_bagging_indices
+        sklearn.ensemble.bagging._parallel_build_estimators =_parallel_build_estimators
+        sklearn.ensemble.bagging._generate_bagging_indices = _generate_bagging_indices
+        super(BlaggingClassifier, self).__init__(
+            base_estimator,
+            n_estimators=n_estimators,
+            max_samples=max_samples,
+            max_features=max_features,
+            bootstrap=bootstrap,
+            bootstrap_features=bootstrap_features,
+            oob_score=oob_score,
+            warm_start=warm_start,
+            n_jobs=n_jobs,
+            random_state=random_state,
+            verbose=verbose)
+
+    def __del__(self):
+        print("BlaggingClassifier.changed back!")
+        sklearn.ensemble.bagging._parallel_build_estimators = self.orig_parallel_build_estimators
+        sklearn.ensemble.bagging._generate_bagging_indices = self.orig_generate_bagging_indices
